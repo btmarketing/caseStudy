68e817fcd5a9ab43f9c52eb2d6ee090e1e1810c2
Andy Sigler <andrewsigler1@gmail.com>
2013-08-14 16:13:44 -0700

    Square size and gutter size are final

--- js/main.js
+++ js/main.js
@@ -129,7 +129,7 @@ function resizeTest(){
 ////////////////////////////////////////////////
 
 var gutter = 5; // the space between each box
-var unitSize = 95; // the size each checker box
+var unitSize = 100; // the size each checker box
 var center; // object holding main container div's screen coordinates
 
 function resized(main){
