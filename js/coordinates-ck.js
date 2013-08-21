32d9653c728d1222220ddbf36113436c0e3a9dd3
Dan Brown <slightlyoffbeat@gmail.com>
2013-08-20 17:54:57 -0700

    removing compiled js

--- js/coordinates-ck.js
+++
@@ -1,4 +0,0 @@
-////////////////////////////////////////////////
-////////////////////////////////////////////////
-////////////////////////////////////////////////
-var dimensions={title:{name:"title",width:2,height:2},big:{name:"big",width:4,height:3},rect_1:{name:"rect_1",width:4,height:2},rect_2:{name:"rect_2",width:4,height:2},unit_1:{name:"unit_1",width:1,height:1},"double":{name:"double",width:2,height:1},long_1:{name:"long_1",width:3,height:1},long_2:{name:"long_2",width:3,height:1}},coordinates=[{title:{l:0,t:0},big:{l:2,t:2},rect_1:{l:2,t:0},rect_2:{l:6,t:1},long_2:{l:6,t:3},"double":{l:7,t:0},long_1:{l:7,t:4},unit_1:{l:6,t:0}},{title:{l:0,t:1},big:{l:6,t:2},rect_1:{l:2,t:0},rect_2:{l:2,t:2},long_2:{l:6,t:1},"double":{l:7,t:0},long_1:{l:3,t:4},unit_1:{l:2,t:4}},{title:{l:0,t:2},big:{l:6,t:0},rect_1:{l:2,t:1},rect_2:{l:6,t:3},long_2:{l:2,t:3},"double":{l:3,t:0},long_1:{l:3,t:4},unit_1:{l:5,t:0}}];
